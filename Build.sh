1d6f0f15470858bf45b70499ad05bbf7cfcbefad
Macoy Madson <macoy@macoy.me>
2021-09-27 09:02:53 -0400

    Initial commit

---
+++ Build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+CAKELISP_DIR=Dependencies/cakelisp
+
+# Build Cakelisp itself
+echo "\n\nCakelisp\n\n"
+cd $CAKELISP_DIR
+./Build.sh || exit $?
+
+cd ../..
+
+echo "\n\nFile Helper\n\n"
+
+CAKELISP=./Dependencies/cakelisp/bin/cakelisp
+
+$CAKELISP --execute --verbose-processes \
+		  src/Config_Linux.cake \
+		  src/AutoColor.cake || exit $?
